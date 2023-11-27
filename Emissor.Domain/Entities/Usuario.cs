3cf91742df873990132a2fa57ceadea45c39d279
Samuel Ives <sivesssl@gmail.com>
2023-11-27 14:54:34 -0300

    Entidades iniciais do banco de dados

---
+++ Emissor.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emissor.Domain.Entities;
+
+public class Usuario
+{
+
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string NomeUsuario { get; set; }
+    public string Senha { get; set; }
+
+}
---
+++ Emissor.Infra/Database/PgContext.cs
@@ -0,0 +1,26 @@
+﻿using Emissor.Infra.Database.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emissor.Application.Database;
+
+public class PgContext : DbContext
+{
+
+    public PgContext(DbContextOptions<PgContext> options) 
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasPostgresExtension("uuid-osp");
+        builder.ApplyConfiguration(new UsuarioMapping());
+    }
+
+}
