931d000a79d57c2cf9141f7436fa163bd7d595df
Samuel Ives <sivesssl@gmail.com>
2023-12-15 19:36:00 -0300

    Angular adicionado

---
+++ emissor-os-front/src/app/components/root/root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root',
+  standalone: true,
+  imports: [],
+  templateUrl: './root.component.html',
+  styleUrl: './root.component.css',
+})
+export class RootComponent {}
